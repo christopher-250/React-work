51c49970b883c0c92b2487fda90a921cb5f0ea38
Christophe Shumbusho <christopheshumbusho69@gmail.com>
2025-05-20 20:27:00 +0200

    React introduction

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+function Header(){
+
+
+    return(
+        <header>
+             <h1>My react work</h1>
+             <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">services</a></li>
+                    <li><a href="#">Contact</a></li>
+
+
+                </ul>
+             </nav>
+             <hr></hr>
+        </header>
+    ); 
+}
+export default Header
