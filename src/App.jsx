706f185b71c85fa9e5e8f3e9f6be3ce323631e67
Christophe Shumbusho <christopheshumbusho69@gmail.com>
2025-05-25 14:19:49 +0200

    Rendering list and add some CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 
 import List from './List.jsx'
 function App() {
+
+  const fruits =[{id:1,name:"apple", calories:95},
+               {id:2,name:"orange" ,calories:45},
+               {id:3,name:"coconut",calories:105},
+               {id:4,name:"pineapple",calories:37}];
+
+  const Vegetables  =[{id:5,name:"carrots", calories:110},
+               {id:6,name:"celery" ,calories:15},
+               {id:7,name:"corn",calories:45},
+               {id:8,name:"broccoli",calories:60}];
+
+  return (
+    <> 
+    {fruits.length > 0 && <List items={fruits} category = "Fruits"/>}
+    {Vegetables.length > 0 && <List items={Vegetables} category = "Vegetables"/>}
+    </>);
   
-  return (<List/>);
 }
 
 export default App
--- src/List.jsx
+++ src/List.jsx
@@ -1,21 +1,33 @@
-function List(){  
-const fruits =[{id:1,name:"apple", calories:95},
-               {id:2,name:"orange" ,calories:45},
-               {id:3,name:"coconut",calories:105},
-               {id:4,name:"pineapple",calories:37}];
-    //fruits.sort((a,b) => a.name.localeCompare(b.name)); // ALPHABETICAL
-    //fruits.sort((a,b) => b.name.localeCompare(a.name));// Reverse alphabetical
-    //fruits.sort((a,b) => a.calories - b.calories);// Numeric order 
-    const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
-    const listItems = lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}>
-                                         {lowCalFruit.name}: &nbsp;
-                                        <b>{lowCalFruit.calories}</b> </li>);
 
-            return(<ol>{listItems}</ol>);
+import PropTypes from 'prop-types';
+function List(props){  
+    const category = props.category;
+    const itemList = props.items;
+    
+    const listItems =  itemList.map(item => <li key={item.id}>
+                                         {item.name}: &nbsp;
+                                        <b>{item.calories}</b> </li>);
+
+            return(
+            <>
+            <h3 className ="list-category">{category}</h3>
+            <ol className ="list-items">{listItems}</ol>
+            </>
+            );
 
 }
 
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({id:PropTypes.number,
+                                              name:PropTypes.string,
+                                              calories: PropTypes.number
+    })) ,                                                   
+}
 
-    
+List.defaultProps = {
+    category:"Category",
+    items:[],
 
+}
 export default List
